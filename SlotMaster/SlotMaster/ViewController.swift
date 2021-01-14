b830e1467a52aaef1d3001831d67fd734ea0e017
rachwongrw <rwrachelwong@gmail.com>
2021-01-14 09:30:08 -0800

    add proj files

---
+++ SlotMaster/SlotMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlotMaster
+//
+//  Created by Rachel Wong on 14/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
