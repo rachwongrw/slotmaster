a402dc7a93b572ed1ca724b26902da0f80c61550
rachwongrw <rwrachelwong@gmail.com>
2021-01-14 11:42:19 -0800

    have randomised img show for each slot

--- SlotMaster/SlotMaster/ViewController.swift
+++ SlotMaster/SlotMaster/ViewController.swift
@@ -8,14 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+  let imageNames: [String] = ["lemon", "dollarsign", "cherries", "grapes", "number7"]
+  
   @IBOutlet weak var Img1: UIImageView!
   @IBOutlet weak var Img2: UIImageView!
   @IBOutlet weak var Img3: UIImageView!
   
   @IBAction func playBtnTapped(_ sender: UIButton) {
-    Img1.image = UIImage(named: "dollarsign")
-    Img2.image = UIImage(named: "dollarsign")
-    Img3.image = UIImage(named: "dollarsign")
+    let slots = [Img1, Img2, Img3]
+    
+    for img in slots {
+      let randomImg1 = imageNames.randomElement()
+      if let unwrapped1 = randomImg1 {
+        img?.image = UIImage(named: unwrapped1)
+      }
+    }
   }
   
   override func viewDidLoad() {
