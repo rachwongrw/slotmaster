6c5b32b3b31fcbb368c6be45c2aa0c5ec9b23ad2
rachwongrw <rwrachelwong@gmail.com>
2021-01-14 13:23:53 -0800

    have labels display points and pt msg

--- SlotMaster/SlotMaster/ViewController.swift
+++ SlotMaster/SlotMaster/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
   let imageNames: [String] = ["lemon", "dollarsign", "cherries", "grapes", "number7"]
+  var points: Int = 0
   
   @IBOutlet weak var Img1: UIImageView!
   @IBOutlet weak var Img2: UIImageView!
   @IBOutlet weak var Img3: UIImageView!
+  
   @IBOutlet weak var PointScore: UILabel!
+  @IBOutlet weak var PointsLabel: UILabel!
   
-  
+
   @IBAction func playBtnTapped(_ sender: UIButton) {
     let slots = [Img1, Img2, Img3]
   
@@ -27,10 +30,14 @@ class ViewController: UIViewController {
     }
     
     if Img1.image == Img2.image && Img2.image == Img3.image {
-      print("match: +5 pts")
+      PointsLabel.text = String("You get 5 points!")
     } else if Img1.image == Img2.image || Img2.image == Img3.image || Img3.image == Img1.image {
-      print("match: +2 pts")
+      points += 2
+      PointsLabel.text = String("You get 2 points!")
+    } else {
+      PointsLabel.text = String("")
     }
+    PointScore.text = String(points)
   }
   
   override func viewDidLoad() {
