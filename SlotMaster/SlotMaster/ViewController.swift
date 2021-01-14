ab8894a348c48f6471c9b861deda94b377f99b11
rachwongrw <rwrachelwong@gmail.com>
2021-01-14 11:22:31 -0800

    connect play btn to img

--- SlotMaster/SlotMaster/ViewController.swift
+++ SlotMaster/SlotMaster/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var Img1: UIImageView!
+  @IBOutlet weak var Img2: UIImageView!
+  @IBOutlet weak var Img3: UIImageView!
+  
+  @IBAction func playBtnTapped(_ sender: UIButton) {
+    Img1.image = UIImage(named: "dollarsign")
+    Img2.image = UIImage(named: "dollarsign")
+    Img3.image = UIImage(named: "dollarsign")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
